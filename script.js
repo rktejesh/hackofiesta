f496137133b44895356fef4ff0be3f29d8d191c8
manishreddy6305 <manishreddy6305@gmail.com>
2022-03-29 17:05:38 +0530

    updated site

--- script.js
+++ script.js
@@ -143,114 +143,114 @@ const redirectbro=()=>{
   window.open("https://drive.google.com/file/d/1ZPpGbLeGDBcONBVLzcOxdmcPAu6IaVNd/view","_blank")
 }
 
-class parallaxTiltEffect {
+// class parallaxTiltEffect {
 
-    constructor({element, tiltEffect}) {
+//     constructor({element, tiltEffect}) {
   
-      this.element = element;
-      this.container = this.element.querySelector(".container");
-      this.size = [300, 360];
-      [this.w, this.h] = this.size;
+//       this.element = element;
+//       this.container = this.element.querySelector(".container");
+//       this.size = [300, 360];
+//       [this.w, this.h] = this.size;
   
-      this.tiltEffect = tiltEffect;
+//       this.tiltEffect = tiltEffect;
   
-      this.mouseOnComponent = false;
+//       this.mouseOnComponent = false;
   
-      this.handleMouseMove = this.handleMouseMove.bind(this);
-      this.handleMouseEnter = this.handleMouseEnter.bind(this);
-      this.handleMouseLeave = this.handleMouseLeave.bind(this);
-      this.defaultStates = this.defaultStates.bind(this);
-      this.setProperty = this.setProperty.bind(this);
-      this.init = this.init.bind(this);
+//       this.handleMouseMove = this.handleMouseMove.bind(this);
+//       this.handleMouseEnter = this.handleMouseEnter.bind(this);
+//       this.handleMouseLeave = this.handleMouseLeave.bind(this);
+//       this.defaultStates = this.defaultStates.bind(this);
+//       this.setProperty = this.setProperty.bind(this);
+//       this.init = this.init.bind(this);
   
-      this.init();
-    }
+//       this.init();
+//     }
   
-    handleMouseMove(event) {
-      const {offsetX, offsetY} = event;
+//     handleMouseMove(event) {
+//       const {offsetX, offsetY} = event;
   
-      let X;
-      let Y;
+//       let X;
+//       let Y;
   
-      if (this.tiltEffect === "reverse") {
-        X = ((offsetX - (this.w/2)) / 3) / 3;
-        Y = (-(offsetY - (this.h/2)) / 3) / 3;
-      }
+//       if (this.tiltEffect === "reverse") {
+//         X = ((offsetX - (this.w/2)) / 3) / 3;
+//         Y = (-(offsetY - (this.h/2)) / 3) / 3;
+//       }
   
-      else if (this.tiltEffect === "normal") {
-        X = (-(offsetX - (this.w/2)) / 3) / 3;
-        Y = ((offsetY - (this.h/2)) / 3) / 3;
-      }
+//       else if (this.tiltEffect === "normal") {
+//         X = (-(offsetX - (this.w/2)) / 3) / 3;
+//         Y = ((offsetY - (this.h/2)) / 3) / 3;
+//       }
   
-      this.setProperty('--rY', X.toFixed(2));
-      this.setProperty('--rX', Y.toFixed(2));
+//       this.setProperty('--rY', X.toFixed(2));
+//       this.setProperty('--rX', Y.toFixed(2));
   
-      this.setProperty('--bY', (80 - (X/4).toFixed(2)) + '%');
-      this.setProperty('--bX', (50 - (Y/4).toFixed(2)) + '%');
-    }
+//       this.setProperty('--bY', (80 - (X/4).toFixed(2)) + '%');
+//       this.setProperty('--bX', (50 - (Y/4).toFixed(2)) + '%');
+//     }
   
-    handleMouseEnter() {
-      this.mouseOnComponent = true;
-      this.container.classList.add("container--active");
-    }
+//     handleMouseEnter() {
+//       this.mouseOnComponent = true;
+//       this.container.classList.add("container--active");
+//     }
   
-    handleMouseLeave() {
-      this.mouseOnComponent = false;
-      this.defaultStates();
-    }
+//     handleMouseLeave() {
+//       this.mouseOnComponent = false;
+//       this.defaultStates();
+//     }
   
-    defaultStates() {
-      this.container.classList.remove("container--active");
-      this.setProperty('--rY', 0);
-      this.setProperty('--rX', 0);
-      this.setProperty('--bY', '80%');
-      this.setProperty('--bX', '50%');
-    }
+//     defaultStates() {
+//       this.container.classList.remove("container--active");
+//       this.setProperty('--rY', 0);
+//       this.setProperty('--rX', 0);
+//       this.setProperty('--bY', '80%');
+//       this.setProperty('--bX', '50%');
+//     }
   
-    setProperty(p, v) {
-      return this.container.style.setProperty(p, v);
-    }
+//     setProperty(p, v) {
+//       return this.container.style.setProperty(p, v);
+//     }
   
-    init() {
-      this.element.addEventListener('mousemove', this.handleMouseMove);
-      this.element.addEventListener('mouseenter', this.handleMouseEnter);
-      this.element.addEventListener('mouseleave', this.handleMouseLeave);
-    }
+//     init() {
+//       this.element.addEventListener('mousemove', this.handleMouseMove);
+//       this.element.addEventListener('mouseenter', this.handleMouseEnter);
+//       this.element.addEventListener('mouseleave', this.handleMouseLeave);
+//     }
   
-  }
+//   }
   
-  const $hov = e => document.querySelector(e);
+//   const $hov = e => document.querySelector(e);
   
-  const wrap1 = new parallaxTiltEffect({
-    element: $hov('.wrap--1'),
-    tiltEffect: 'reverse'
-  });
+//   const wrap1 = new parallaxTiltEffect({
+//     element: $hov('.wrap--1'),
+//     tiltEffect: 'reverse'
+//   });
   
-  const wrap2 = new parallaxTiltEffect({
-    element: $hov('.wrap--2'),
-    tiltEffect: 'reverse'
-  });
+//   const wrap2 = new parallaxTiltEffect({
+//     element: $hov('.wrap--2'),
+//     tiltEffect: 'reverse'
+//   });
   
-  const wrap3 = new parallaxTiltEffect({
-    element: $hov('.wrap--3'),
-    tiltEffect: 'reverse'
-  });
-  const wrap4 = new parallaxTiltEffect({
-    element: $hov('.wrap--4'),
-    tiltEffect: 'reverse'
-  });
-  const wrap5 = new parallaxTiltEffect({
-    element: $hov('.wrap--5'),
-    tiltEffect: 'reverse'
-  });
-  const wrap6 = new parallaxTiltEffect({
-    element: $hov('.wrap--6'),
-    tiltEffect: 'reverse'
-  });
-  const wrap7 = new parallaxTiltEffect({
-    element: $hov('.wrap--7'),
-    tiltEffect: 'reverse'
-  });
+//   const wrap3 = new parallaxTiltEffect({
+//     element: $hov('.wrap--3'),
+//     tiltEffect: 'reverse'
+//   });
+//   const wrap4 = new parallaxTiltEffect({
+//     element: $hov('.wrap--4'),
+//     tiltEffect: 'reverse'
+//   });
+//   const wrap5 = new parallaxTiltEffect({
+//     element: $hov('.wrap--5'),
+//     tiltEffect: 'reverse'
+//   });
+//   const wrap6 = new parallaxTiltEffect({
+//     element: $hov('.wrap--6'),
+//     tiltEffect: 'reverse'
+//   });
+//   const wrap7 = new parallaxTiltEffect({
+//     element: $hov('.wrap--7'),
+//     tiltEffect: 'reverse'
+//   });
 
   function themename(){
       let t=document.getElementsByClassName("theme__item--details")[0];
@@ -398,21 +398,6 @@ function pmdo5(){
   let t1=document.getElementsByClassName("trophy")[4];
   t1.style.opacity=0.3;
 }
-const items = document.querySelectorAll(".accordion button");
-
-function toggleAccordion() {
-  const itemToggle = this.getAttribute('aria-expanded');
-  
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
-  }
-  
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
-  }
-}
-
-items.forEach(item => item.addEventListener('click', toggleAccordion));
 
 $(".list").click(function () {
   const value = $(this).attr("data-filter");
@@ -430,4 +415,25 @@ $(".list").click(function () {
       .filter("." + value)
       .fadeIn(400);
   }
+});
+
+
+const btnitems=document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  console.log("encountered");
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < btnitems.length; i++) {
+    btnitems[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+btnitems.forEach((item) => {
+  console.log(item);
+  item.addEventListener('click', toggleAccordion)
 });
